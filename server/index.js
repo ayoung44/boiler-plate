c8081796d4dc90293a14bd8027bbfaf10b301f55
ayoung44 <a_young__@naver.com>
2020-04-13 22:21:30 +0900

    비밀정보 보호

--- server/index.js
+++ server/index.js
@@ -4,16 +4,46 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
+const config = require('../config/key');
+
+const { User } = require('../models/User');  
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
+
+
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dbUser:1234@cluster0-wywue.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, 
     useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
+
+
+
+
+
 app.get('/', (req, res) => res.send('hello'));
 
-
+app.post('/register', (req, res) => {
+    // 클라이언트에서 가져오는 회원 가입 정보
+    // 데이터베이스에 넣기 
+    const user = new User(req.body);
+    
+    // mongodb  method
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 
 
